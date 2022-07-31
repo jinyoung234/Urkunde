34151639d9e1da8dca11f89191fafdc5e40e877b
jinyoung <sm099@naver.com>
2022-07-31 22:36:27 +0900

    feat : state management file implement(-ing)

--- front-end/src/utils/storage.ts
+++ front-end/src/utils/storage.ts
@@ -1,11 +1,31 @@
 import { atom } from "recoil";
 
+export interface ISetInfo {
+    id : number,
+    question : string,
+    answer : string
+}
+
+// 모달의 상태 관리
 export const modalState = atom({
     key : "modalState",
     default: false,
 })
 
+// 모드 변경 상태 관리
 export const lightMode = atom ({
     key: "lightMode",
     default : true,
+})
+
+// 전체 문제 갯수 상태 관리
+export const questionNum = atom({
+    key: "questionNum",
+    default : 0,
+})
+
+// 문제-답 세트 상태 관리
+export const questionSet = atom<ISetInfo[]>({
+    key: "qustionSet",
+    default : [],
 })
