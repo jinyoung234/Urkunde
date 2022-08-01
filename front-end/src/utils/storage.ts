c8cca023fc818c9363270b6fda50a54de4a49ad1
jinyoung <sm099@naver.com>
2022-08-01 17:49:25 +0900

    feat : saving atoms(data) in localstorage

--- front-end/src/utils/storage.ts
+++ front-end/src/utils/storage.ts
@@ -1,4 +1,12 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const {persistAtom} = recoilPersist({
+    key: 'questions',
+    storage : localStorage,
+})
+
+
 
 export interface ISetInfo {
     id : number,
@@ -22,10 +30,12 @@ export const lightMode = atom ({
 export const questionNum = atom({
     key: "questionNum",
     default : 0,
+    effects_UNSTABLE: [persistAtom],
 })
 
 // 문제-답 세트 상태 관리
 export const questionSet = atom<ISetInfo[]>({
     key: "qustionSet",
     default : [],
+    effects_UNSTABLE: [persistAtom],
 })
