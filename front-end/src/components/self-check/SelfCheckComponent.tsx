201efe0e4cd1ffb27d3d39658ea15f590770e582
jinyoung <sm099@naver.com>
2022-08-21 23:23:49 +0900

    Refactor : combine atoms and molecules in self check component

---
+++ front-end/src/components/self-check/SelfCheckComponent.tsx
@@ -0,0 +1,39 @@
+import { useRecoilValue } from "recoil"
+import { questionNum, weeklyCheck, currentIndex, answerState } from "src/utils/storage"
+import { Container } from "src/components/commons/Commons";
+import { useEffect } from "react";
+import BackButton from "src/components/commons/BackButton";
+import SelfCheckTitle from "src/components/self-check/atoms/SelfCheckTitle";
+import SelfCheckIndex from "src/components/self-check/atoms/SelfCheckIndex";
+import EmptySelfCheckForm from "src/components/self-check/molecules/EmptySelfCheckForm";
+import GoResultLink from "src/components/self-check/atoms/GoResultLink";
+import useTimer from "src/hooks/useTimer";
+import QuizForm from "src/components/self-check/molecules/QuizForm";
+
+export default function SelfCheckComponent() {
+    const week = useRecoilValue(weeklyCheck);
+
+    let {dayOfWeek} = useTimer();
+
+    const quesionNumber = useRecoilValue(questionNum);
+    
+    const answer = useRecoilValue(answerState);
+
+    useEffect(() => {
+    },[answer])
+
+    const curr = useRecoilValue(currentIndex);
+    return (
+        <Container style={{height: curr + 1 === quesionNumber ? '113vh' : '106vh'}}>
+            <BackButton
+                url={"/create"}
+                isSelfCheck = {true}
+            />
+            <SelfCheckTitle date={week[dayOfWeek].date}/>
+            <SelfCheckIndex/>
+            <GoResultLink/>
+            <EmptySelfCheckForm/>
+            <QuizForm/>
+        </Container>
+    )
+}
