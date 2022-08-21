9d32850dedb3a8553329e87eddf1160af0ba97dd
jinyoung <sm099@naver.com>
2022-08-22 01:04:36 +0900

    Rename : modify import route

--- front-end/src/utils/Routes.tsx
+++ front-end/src/utils/Routes.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from "../components/NavBar";
-import Check from "../routes/Check";
-import CreateQuestion from "../routes/CreateQuestion";
-import Home from "../routes/Home";
-import ModifyQuestion from "../routes/ModifyQuestion";
-import SelfCheck from "../routes/SelfCheck";
-import SelfCheckResult from "../routes/SelfCheckResult";
+import NavBar from "src/components/commons/NavBar";
+import Check from "src/routes/Check";
+import CreateQuestion from "src/routes/CreateQuestion";
+import Home from "src/routes/Home";
+import ModifyQuestion from "src/routes/ModifyQuestion";
+import SelfCheck from "src/routes/SelfCheck";
+import SelfCheckResult from "src/routes/SelfCheckResult";
 
 function Router() {
     return (
