0d54667d6c307e3aa112569b470ce7941f3a0b66
jinyoung <sm099@naver.com>
2022-08-21 23:27:58 +0900

    fix : fix issue after history back

--- front-end/src/components/commons/BackButton.tsx
+++ front-end/src/components/commons/BackButton.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { answerButtonState, correctNum, currentIndex, incorrectNum, isAnswer, sliderMotionChange } from "src/utils/storage";
 import styled from "styled-components";
 
 export const BackCircle = styled.div`
@@ -17,21 +19,49 @@ export const BackCircle = styled.div`
 
     interface UrlProps {
         url : string
+        isSelfCheck ?: boolean
     }
 
-    export default function BackButton({url} : UrlProps) : React.ReactElement {
+    export default function BackButton({url, isSelfCheck} : UrlProps) : React.ReactElement {
     
-    // 뒤로가기 버튼
+    const setChangeMotion = useSetRecoilState(sliderMotionChange);
+    const setCurrentIndex = useSetRecoilState(currentIndex);
+    const setAnswerState = useSetRecoilState(isAnswer);
+    const setIncorrectNum = useSetRecoilState(incorrectNum);
+    const setCorrectNum = useSetRecoilState(correctNum); 
+    const setButtonState = useSetRecoilState(answerButtonState); 
+
+
     const moveBack = useNavigate();
     const handleBackCircleClick = () => {
         moveBack(url);
     }
 
+    const handleSelfCheckBackCircleClick = () => {
+        setChangeMotion(false);
+        setCurrentIndex(0);
+        setAnswerState(true);
+        moveBack(url);
+        setIncorrectNum(0);
+        setCorrectNum(0);
+        setButtonState(false);
+    }
+
     return (
-        <BackCircle
-                onClick = {handleBackCircleClick}
+        <>
+        { isSelfCheck ?
+            (<BackCircle
+                style={{left:'25%'}}
+                onClick = {handleSelfCheckBackCircleClick}
             >
                 <img style={{marginRight:'2px', width: '50%'}} src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPHN2ZyB3aWR0aD0iNzUycHQiIGhlaWdodD0iNzUycHQiIHZlcnNpb249IjEuMSIgdmlld0JveD0iMCAwIDc1MiA3NTIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiA8ZGVmcz4KICA8Y2xpcFBhdGggaWQ9ImEiPgogICA8cGF0aCBkPSJtMjQzIDEzOS4yMWgyNjZ2NDczLjU4aC0yNjZ6Ii8+CiAgPC9jbGlwUGF0aD4KIDwvZGVmcz4KIDxnIGNsaXAtcGF0aD0idXJsKCNhKSI+CiAgPHBhdGggZD0ibTI1MS41OCAzNTQuNzMgMjA3LjAyLTIwNy4wMmMxMi4wNTEtMTEuMzQ0IDMwLjQ4NC0xMS4zNDQgNDEuODI4IDBzMTEuMzQ0IDI5Ljc3NyAwIDQxLjgyOGwtMTg2LjQ1IDE4Ni40NSAxODYuNDUgMTg2LjQ1YzExLjM0NCAxMS4zNDQgMTEuMzQ0IDMwLjQ4NCAwIDQxLjgyOHMtMjkuNzc3IDExLjM0NC00MS44MjggMGwtMjA3LjAyLTIwNy43MmMtMTEuMzQ0LTExLjM0NC0xMS4zNDQtMjkuNzc3IDAtNDEuODI4eiIgZmlsbC1ydWxlPSJldmVub2RkIi8+CiA8L2c+Cjwvc3ZnPgo=" />
-        </BackCircle>
+            </BackCircle>) :
+            (<BackCircle
+            onClick = {handleBackCircleClick}
+            >
+            <img style={{marginRight:'2px', width: '50%'}} src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPHN2ZyB3aWR0aD0iNzUycHQiIGhlaWdodD0iNzUycHQiIHZlcnNpb249IjEuMSIgdmlld0JveD0iMCAwIDc1MiA3NTIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiA8ZGVmcz4KICA8Y2xpcFBhdGggaWQ9ImEiPgogICA8cGF0aCBkPSJtMjQzIDEzOS4yMWgyNjZ2NDczLjU4aC0yNjZ6Ii8+CiAgPC9jbGlwUGF0aD4KIDwvZGVmcz4KIDxnIGNsaXAtcGF0aD0idXJsKCNhKSI+CiAgPHBhdGggZD0ibTI1MS41OCAzNTQuNzMgMjA3LjAyLTIwNy4wMmMxMi4wNTEtMTEuMzQ0IDMwLjQ4NC0xMS4zNDQgNDEuODI4IDBzMTEuMzQ0IDI5Ljc3NyAwIDQxLjgyOGwtMTg2LjQ1IDE4Ni40NSAxODYuNDUgMTg2LjQ1YzExLjM0NCAxMS4zNDQgMTEuMzQ0IDMwLjQ4NCAwIDQxLjgyOHMtMjkuNzc3IDExLjM0NC00MS44MjggMGwtMjA3LjAyLTIwNy43MmMtMTEuMzQ0LTExLjM0NC0xMS4zNDQtMjkuNzc3IDAtNDEuODI4eiIgZmlsbC1ydWxlPSJldmVub2RkIi8+CiA8L2c+Cjwvc3ZnPgo=" />
+            </BackCircle>)
+        }
+        </>
     )
 }
