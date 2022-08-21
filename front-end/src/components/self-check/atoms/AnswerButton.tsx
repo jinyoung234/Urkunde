b8259d43b9398fdfb52fb9c83a3348a56d9b09a6
jinyoung <sm099@naver.com>
2022-08-21 23:13:45 +0900

    Refactor : separtate atoms(AnswerButton) in selfcheck component

---
+++ front-end/src/components/self-check/atoms/AnswerButton.tsx
@@ -0,0 +1,34 @@
+import { useRecoilValue } from "recoil";
+import { Correct, Incorrect, ModifyPageTitle } from "src/components/commons/Commons";
+import { Button } from "src/components/commons/PrimaryButton";
+import useAnswerCheck from "src/hooks/useAnswerCheck"
+import { answerButtonState, isCorrect } from "src/utils/storage";
+
+interface AnswerButtonProps {
+    answer : string;
+}
+
+export default function AnswerButton({answer} : AnswerButtonProps) {
+    const isWriteAnswer = useRecoilValue(answerButtonState);
+    const {handleAnswerCheck} = useAnswerCheck();
+    const isCorrectAnswer = useRecoilValue(isCorrect);
+
+    return(
+        <>
+        {!isWriteAnswer && 
+            <Button 
+                onClick={() => handleAnswerCheck(answer)}
+                style={{margin:'0px 0px', marginTop : '20px'}}
+            >
+                정답 확인
+            </Button>
+        }
+        {isWriteAnswer && 
+            <ModifyPageTitle style={{marginTop: '15px', display:'flex', flexDirection:'column'}}>
+                {isCorrectAnswer ? <Correct>정답입니다 :)</Correct> : <Incorrect>틀렸습니다 :)</Incorrect>}
+                {answer}
+            </ModifyPageTitle>
+        }
+        </>
+    )
+}
