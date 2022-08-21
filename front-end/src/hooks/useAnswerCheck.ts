a65a7801943f27b4ab8ec494857168297a6c72fa
jinyoung <sm099@naver.com>
2022-08-21 23:21:48 +0900

    feat : create custom hook for answer check(self-check component)

---
+++ front-end/src/hooks/useAnswerCheck.ts
@@ -0,0 +1,30 @@
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { answerButtonState, answerState, correctNum, incorrectNum, isCorrect } from "src/utils/storage";
+
+export default function useAnswerCheck() {
+    
+    const userAnswer = useRecoilValue(answerState);
+    const setButtonState = useSetRecoilState(answerButtonState);
+    const increaseCorrectQuestion = useSetRecoilState(correctNum);
+    const increaseIncorrectQuestion = useSetRecoilState(incorrectNum);
+    const setIsCollect = useSetRecoilState(isCorrect);
+
+    const handleAnswerCheck = (answer : string) => {
+        if(userAnswer === '') {
+            alert('답을 작성해주세요 :)');
+            return
+        } 
+        setButtonState(true);
+        if(userAnswer === answer) {
+                setIsCollect(true);
+                increaseCorrectQuestion((prev: number) => prev+1);
+        } else {
+                setIsCollect(false);
+                increaseIncorrectQuestion((prev : number) => prev+1);
+        }
+    }
+
+    return {
+        handleAnswerCheck,
+    }
+}
