47fb813993641afa011276b815ab9376f21eeba3
jinyoung <sm099@naver.com>
2022-08-21 23:22:05 +0900

    feat : create custom hook for silder(self-check component)

---
+++ front-end/src/hooks/useSlider.ts
@@ -0,0 +1,34 @@
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { answerButtonState, currentIndex, isAnswer, isCorrect, questionSet, sliderMotionChange } from "src/utils/storage";
+
+export default function useSlider() {
+    const setChangeMotion = useSetRecoilState(sliderMotionChange);
+
+    const setCurrentIndex = useSetRecoilState(currentIndex);
+
+    const setAnswerState = useSetRecoilState(isAnswer);
+
+    const questions = useRecoilValue(questionSet);
+
+    const setButtonState = useSetRecoilState(answerButtonState);
+
+
+    const handleLeftSlider = () => {
+        setChangeMotion(true);
+        setCurrentIndex((prev) => prev === 0 ? prev : prev - 1);
+        setAnswerState(false);
+        setButtonState(false);
+    }
+
+    const handleRightSlider = () => {
+        setChangeMotion(false);
+        setCurrentIndex((prev) => prev === questions.length-1 ? prev : prev + 1);
+        setAnswerState(false);
+        setButtonState(false);
+    }
+
+    return {
+        handleLeftSlider,
+        handleRightSlider
+    }
+}
