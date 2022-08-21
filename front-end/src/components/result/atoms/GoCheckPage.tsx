59e1e1c33fd6a99a373737c47792b87c68433670
jinyoung <sm099@naver.com>
2022-08-22 00:58:45 +0900

    Refactor : separate in atoms(Go check Page) in self-check result component

---
+++ front-end/src/components/result/atoms/GoCheckPage.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { SubTitle } from "src/components/commons/Commons";
+import { answerState } from "src/utils/storage";
+
+export default function GoCheckPage() {
+    const setAnswer = useSetRecoilState(answerState); 
+
+    const handleGoPage = () => {
+        setAnswer('')
+    }
+    return(
+        <SubTitle style={{fontWeight:'500'}}
+            onClick={handleGoPage}
+        >
+            <Link 
+                to="/check">체크 페이지로 이동하기
+            </Link>
+        </SubTitle>
+    )
+}
