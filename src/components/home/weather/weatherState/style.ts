114427b63b1cc3673cf93b6524bb5e4eb336a4b7
jinyoung <sm099@naver.com>
2022-12-03 14:12:32 +0900

    Refactor : Refactoring WeatherState Component

---
+++ src/components/home/weather/weatherState/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const WeatherStateWrapper = styled.div`
+  #cloud {
+    width: 10rem;
+    height: 10rem;
+  }
+  #clear {
+    width: 7em;
+    height: 7em;
+    margin-top: 1rem;
+  }
+  #rain {
+    width: 7em;
+    height: 7em;
+    margin-top: 2rem;
+  }
+`;
