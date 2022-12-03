35943980b66fc2d09ab7588ba79a81436956aead
jinyoung <sm099@naver.com>
2022-12-03 23:59:19 +0900

    Refactor : Refactoring PageTitle Component

--- src/components/commons/pageTitle/index.tsx
+++ src/components/commons/pageTitle/index.tsx
@@ -1,16 +1,30 @@
-import { Title } from "./style";
-import { SubTitle } from "./style";
+import BackButton from "../button/BackButton";
+import { PageTitleWrapper, TitleWrapper } from "./style";
+import { SubTitleWrapper } from "./style";
 
 interface PageTitleProps {
   title: string;
   subtitle: string;
+  url: string;
 }
 
-export default function PageTitle({ title, subtitle }: PageTitleProps) {
+export default function PageTitle({ title, subtitle, url }: PageTitleProps) {
   return (
-    <>
-      <Title>{title}</Title>
-      <SubTitle>{subtitle}</SubTitle>
-    </>
+    <PageTitleWrapper>
+      {url === "" ? (
+        <>
+          <TitleWrapper>{title}</TitleWrapper>
+          <SubTitleWrapper>{subtitle}</SubTitleWrapper>
+        </>
+      ) : (
+        <>
+          <TitleWrapper>
+            <BackButton url={url} />
+            <span>{title}</span>
+          </TitleWrapper>
+          <SubTitleWrapper>{subtitle}</SubTitleWrapper>
+        </>
+      )}
+    </PageTitleWrapper>
   );
 }
--- src/components/commons/pageTitle/style.ts
+++ src/components/commons/pageTitle/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Title = styled.div`
+export const TitleWrapper = styled.div`
   background-color: rgba(0, 0, 0, 0);
   display: flex;
   align-items: center;
@@ -9,10 +9,20 @@ export const Title = styled.div`
   font-weight: 500;
   margin: 30px 0px;
   color: ${(props) => props.theme.textColor};
+  span {
+    padding-top: 0.6rem;
+  }
 `;
 
-export const SubTitle = styled.div`
+export const SubTitleWrapper = styled.div`
   font-size: 20px;
   margin-bottom: 30px;
   color: ${(props) => props.theme.textColor};
+`;
+
+export const PageTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
