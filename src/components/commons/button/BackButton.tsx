dabf6de9664e0f869514b68e871c5c338daf5130
jinyoung <sm099@naver.com>
2022-12-03 23:58:55 +0900

    Refactor : Refactoring BackButton Component

--- src/components/commons/button/BackButton.tsx
+++ src/components/commons/button/BackButton.tsx
@@ -8,23 +8,9 @@ import {
   isAnswer,
   sliderMotionChange,
 } from "src/atoms";
-import styled from "styled-components";
+import { BackButtonWrapper } from "./style";
 
-export const BackCircle = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-  position: absolute;
-  background-color: white;
-  top: 110px;
-  left: 20%;
-  width: 4%;
-  height: 8vh;
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.15), 0 1px 3px rgba(0, 0, 0, 0.08);
-`;
-
-interface UrlProps {
+interface BackButtonProps {
   url: string;
   isSelfCheck?: boolean;
 }
@@ -32,7 +18,7 @@ interface UrlProps {
 export default function BackButton({
   url,
   isSelfCheck,
-}: UrlProps): React.ReactElement {
+}: BackButtonProps): React.ReactElement {
   const setChangeMotion = useSetRecoilState(sliderMotionChange);
   const setCurrentIndex = useSetRecoilState(currentIndex);
   const setAnswerState = useSetRecoilState(isAnswer);
@@ -58,22 +44,22 @@ export default function BackButton({
   return (
     <>
       {isSelfCheck ? (
-        <BackCircle
-          style={{ left: "25%" }}
+        <BackButtonWrapper
+          isSelfCheck={true}
           onClick={handleSelfCheckBackCircleClick}
         >
           <img
-            style={{ marginRight: "2px", width: "50%" }}
             src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPHN2ZyB3aWR0aD0iNzUycHQiIGhlaWdodD0iNzUycHQiIHZlcnNpb249IjEuMSIgdmlld0JveD0iMCAwIDc1MiA3NTIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiA8ZGVmcz4KICA8Y2xpcFBhdGggaWQ9ImEiPgogICA8cGF0aCBkPSJtMjQzIDEzOS4yMWgyNjZ2NDczLjU4aC0yNjZ6Ii8+CiAgPC9jbGlwUGF0aD4KIDwvZGVmcz4KIDxnIGNsaXAtcGF0aD0idXJsKCNhKSI+CiAgPHBhdGggZD0ibTI1MS41OCAzNTQuNzMgMjA3LjAyLTIwNy4wMmMxMi4wNTEtMTEuMzQ0IDMwLjQ4NC0xMS4zNDQgNDEuODI4IDBzMTEuMzQ0IDI5Ljc3NyAwIDQxLjgyOGwtMTg2LjQ1IDE4Ni40NSAxODYuNDUgMTg2LjQ1YzExLjM0NCAxMS4zNDQgMTEuMzQ0IDMwLjQ4NCAwIDQxLjgyOHMtMjkuNzc3IDExLjM0NC00MS44MjggMGwtMjA3LjAyLTIwNy43MmMtMTEuMzQ0LTExLjM0NC0xMS4zNDQtMjkuNzc3IDAtNDEuODI4eiIgZmlsbC1ydWxlPSJldmVub2RkIi8+CiA8L2c+Cjwvc3ZnPgo="
+            alt="back"
           />
-        </BackCircle>
+        </BackButtonWrapper>
       ) : (
-        <BackCircle onClick={handleBackCircleClick}>
+        <BackButtonWrapper onClick={handleBackCircleClick}>
           <img
-            style={{ marginRight: "2px", width: "50%" }}
             src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPHN2ZyB3aWR0aD0iNzUycHQiIGhlaWdodD0iNzUycHQiIHZlcnNpb249IjEuMSIgdmlld0JveD0iMCAwIDc1MiA3NTIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiA8ZGVmcz4KICA8Y2xpcFBhdGggaWQ9ImEiPgogICA8cGF0aCBkPSJtMjQzIDEzOS4yMWgyNjZ2NDczLjU4aC0yNjZ6Ii8+CiAgPC9jbGlwUGF0aD4KIDwvZGVmcz4KIDxnIGNsaXAtcGF0aD0idXJsKCNhKSI+CiAgPHBhdGggZD0ibTI1MS41OCAzNTQuNzMgMjA3LjAyLTIwNy4wMmMxMi4wNTEtMTEuMzQ0IDMwLjQ4NC0xMS4zNDQgNDEuODI4IDBzMTEuMzQ0IDI5Ljc3NyAwIDQxLjgyOGwtMTg2LjQ1IDE4Ni40NSAxODYuNDUgMTg2LjQ1YzExLjM0NCAxMS4zNDQgMTEuMzQ0IDMwLjQ4NCAwIDQxLjgyOHMtMjkuNzc3IDExLjM0NC00MS44MjggMGwtMjA3LjAyLTIwNy43MmMtMTEuMzQ0LTExLjM0NC0xMS4zNDQtMjkuNzc3IDAtNDEuODI4eiIgZmlsbC1ydWxlPSJldmVub2RkIi8+CiA8L2c+Cjwvc3ZnPgo="
+            alt="back"
           />
-        </BackCircle>
+        </BackButtonWrapper>
       )}
     </>
   );
--- src/components/commons/button/style.ts
+++ src/components/commons/button/style.ts
@@ -2,11 +2,28 @@ import styled from "styled-components";
 
 export const ButtonWrapper = styled.button<{ size: string }>`
   margin: 50px 0px;
-  width: ${(props) => (props.size === "sm" ? "12rem" : "")};
+  width: ${(props) => (props.size === "sm" ? "10rem" : "")};
   border: 0px;
-  height: ${(props) => (props.size === "sm" ? "5rem" : "")};
+  height: ${(props) => (props.size === "sm" ? "3rem" : "")};
   border-radius: 10px;
   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.15), 0 1px 3px rgba(0, 0, 0, 0.08);
   background-color: #3c73cf;
   color: white;
+`;
+
+export const BackButtonWrapper = styled.div<{ isSelfCheck?: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  background-color: white;
+  width: 6rem;
+  height: 6rem;
+  position: absolute;
+  left: ${(props) => (props.isSelfCheck ? "25%" : "18%")};
+  box-shadow: 0 4rem 6rem rgba(50, 50, 93, 0.15),
+    0 1rem 3rem rgba(0, 0, 0, 0.08);
+  img {
+    width: 3rem;
+  }
 `;
