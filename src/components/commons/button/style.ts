b9926ef7a1c5827634ab7f9677fe5942d2b43797
jinyoung <sm099@naver.com>
2022-12-04 12:10:48 +0900

    style : Modify primary button style

--- src/components/commons/button/style.ts
+++ src/components/commons/button/style.ts
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 export const ButtonWrapper = styled.button<{ size: string }>`
-  margin: 50px 0px;
+  margin: 4rem 0px;
   width: ${(props) => (props.size === "sm" ? "10rem" : "")};
   border: 0px;
   height: ${(props) => (props.size === "sm" ? "3rem" : "")};
-  border-radius: 10px;
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.15), 0 1px 3px rgba(0, 0, 0, 0.08);
+  border-radius: 1rem;
+  box-shadow: 0 4rem 6rem rgba(50, 50, 93, 0.15),
+    0 1rem 3rem rgba(0, 0, 0, 0.08);
   background-color: #3c73cf;
   color: white;
 `;
