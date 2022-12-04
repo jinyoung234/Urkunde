8b5c926b9fbae2badd429249c4596a0eb44020bb
jinyoung <sm099@naver.com>
2022-12-04 13:20:12 +0900

    Refactor : Refactoring CreateQuestion Component

---
+++ src/components/create/createQuestion/index.tsx
@@ -0,0 +1,45 @@
+import ContainerTitle from "src/components/commons/containerTitle";
+import Input from "./input";
+import QuestionNumber from "./questionNumber";
+import ResetButton from "./button/ResetButton";
+import MovePageButton from "./button/MovePageButton";
+import CreateButton from "./button/CreateButton";
+import TextArea from "./textArea";
+import useCreate from "src/hooks/useCreate";
+import useLoadAllQuestion from "src/hooks/useLoadAllQuestion";
+import useReset from "src/hooks/useReset";
+import useQuestionSet from "src/hooks/useSetQuestion";
+import { CreateQuestionSubWrapper, CreateQuestionWrapper } from "./style";
+
+export default function CreateQuestion() {
+  const { questionNumber } = useLoadAllQuestion();
+
+  const { handleCreateQuestion, question, answer } = useCreate();
+
+  const { handleResetButton } = useReset();
+
+  const { addQuestion, addAnswer } = useQuestionSet();
+  return (
+    <CreateQuestionWrapper>
+      <ContainerTitle title="Create Question" />
+      <CreateQuestionSubWrapper>
+        <Input changeFn={addQuestion} />
+        <TextArea changeFn={addAnswer} />
+        <QuestionNumber questionNumber={questionNumber} />
+        <CreateButton
+          createQuesitonFn={handleCreateQuestion}
+          content={"문제 생성"}
+          question={question}
+          answer={answer}
+        />
+        <MovePageButton url={"/modify"} content={"문제 수정"} />
+        <ResetButton resetFn={handleResetButton} content={"문제 리셋"} />
+        <MovePageButton
+          url={"/self-check"}
+          content={"점검하러 가기"}
+          isCheck={true}
+        />
+      </CreateQuestionSubWrapper>
+    </CreateQuestionWrapper>
+  );
+}
---
+++ src/components/create/createQuestion/style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const CreateQuestionWrapper = styled.div`
+  background-color: white;
+  border: 0px;
+  border-radius: 4rem;
+  width: 75%;
+  min-height: 90vh;
+  padding: 4rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 0.4rem 0.6rem rgba(50, 50, 93, 0.2),
+    0 0.1rem 0.3rem rgba(0, 0, 0, 0.12);
+`;
+
+export const CreateQuestionSubWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  height: 75vh;
+  border: 0;
+`;
